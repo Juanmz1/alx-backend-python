26f0c3fcc3fa4b9e1a9907dcb5aebc9cb36e2fc2
juanmz1 <juanmzii48@gmail.com>
2023-12-07 02:05:51 -0800

    task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns the sum.
+
+    Parameters:
+    - a (float): The first number.
+    - b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
+
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Description: takes a float multiplier as argument and returns a function
+'''
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    ''' Return function that multiplies float by `multiplier`. '''
+    return lambda x: x * multiplier
